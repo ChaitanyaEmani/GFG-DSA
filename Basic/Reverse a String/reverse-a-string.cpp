f3da073d917a7c4288a099582432337c31621dd3
ChaitanyaEmani <131893680+ChaitanyaEmani@users.noreply.github.com>
2023-09-26 16:07:33 +0530

    Updated Solution - GfG to GitHub

--- Basic/Reverse a String/reverse-a-string.cpp
+++ Basic/Reverse a String/reverse-a-string.cpp
@@ -15,7 +15,10 @@ class Solution
     string reverseWord(string str)
     {
         // Your code goes here
-        reverse(str.begin(),str.end());
+        int n = str.size();
+     for(int i=0;i<n/2;i++){
+         swap(str[i], str[n-i-1]);
+     }
         return str;
     }
 };
