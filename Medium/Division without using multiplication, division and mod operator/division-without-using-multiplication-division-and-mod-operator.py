7e6eaf4f83f461272443a8734c61334d1b5951a1
ChaitanyaEmani <131893680+ChaitanyaEmani@users.noreply.github.com>
2023-10-02 20:24:50 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Division without using multiplication, division and mod operator/division-without-using-multiplication-division-and-mod-operator.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+
+class Solution:
+    def divide(self, a, b):
+        #code here
+        return int (a/b)
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        inp = list(map(int,input().split())) 
+        
+        a=inp[0]
+        b=inp[1]
+        
+        ob=Solution()
+        
+        print(ob.divide(a,b))
+        
+# } Driver Code Ends
