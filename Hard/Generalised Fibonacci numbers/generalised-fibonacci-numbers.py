7db18402541174705ca3e01deef0ddfdb80f58fc
ChaitanyaEmani <131893680+ChaitanyaEmani@users.noreply.github.com>
2024-03-12 20:35:19 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Generalised Fibonacci numbers/generalised-fibonacci-numbers.py
@@ -0,0 +1,32 @@
+#User function Template for python3
+import numpy as np
+class Solution:
+    def genFibNum(self, a, b, c, n, m):
+        # code here 
+     
+        if n<=2:
+            return 1
+        res = np.array([[1,0,0],[0,1,0],[0,0,1]])
+        genFib = np.array([[a,b,c],[1,0,0],[0,0,1]])
+        
+        n-=2
+        while n:
+            if n&1:
+                res = np.matmul(res, genFib)%m
+            genFib = np.matmul(genFib, genFib)%m
+            n>>=1
+        return sum(res[0])%m
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        a,b,c,n,m=map(int,input().split())
+        
+        ob = Solution()
+        print(ob.genFibNum(a,b,c,n,m))
+# } Driver Code Ends
